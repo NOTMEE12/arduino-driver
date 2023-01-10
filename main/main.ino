9f46e8d1decb89e4346cf79887ffe3899cf3f3d8
NOTMEE12 <103068655+NOTMEE12@users.noreply.github.com>
2023-01-10 19:45:42 +0100

    fixing bugs

--- main/main.ino
+++ main/main.ino
@@ -11,40 +11,38 @@ git push -u origin master
 
 */
 
-
-
-int Front_right = 1;
+int Front_right = 4;
 int Front_left = 5;
 int Back_right = 3;
-int Back_left = 4;
+int Back_left = 1;
 
 
-void moveMotors(String direction, double speed=0.1){
+void moveMotors(String direction){
   bool condition_met = false;
   // Front and all similiar directions
-  if(direction == 'Front-left'){
+  if(direction == "Front-left"){
     digitalWrite(Front_right, HIGH);
     condition_met = true;
   }
-  else if(direction == 'Front-right'){
+  else if(direction == "Front-right"){
     digitalWrite(Front_left, HIGH);
     condition_met = true;
   }
-  else if(direction == 'Front'){
+  else if(direction == "Front"){
     condition_met = true;
     digitalWrite(Front_right, HIGH);
     digitalWrite(Front_left, HIGH);
   }
   // Back and all similiar directions
-  else if(direction == 'Back-left'){
+  else if(direction == "Back-left"){
     digitalWrite(Back_right, HIGH);
     condition_met = true;
   }
-  else if(direction == 'Back-right'){
+  else if(direction == "Back-right"){
     digitalWrite(Back_left, HIGH);
     condition_met = true;
   }
-  else if(direction == 'Back'){
+  else if(direction == "Back"){
     digitalWrite(Back_left, HIGH);
     digitalWrite(Back_right, HIGH);
     condition_met = true;
@@ -60,7 +58,7 @@ void moveMotors(String direction, double speed=0.1){
 
 
 void setup() {
-  //String html = "<!DOCTYPE html><html><head><title>Arduino controller</title><style>.main {float: center;align: center;}</style></head><body><div class='main'><div><button> 🢄 </button><button> 🢁 </button><button> 🢅 </button></div><div><button> 🢀 </button><button> .. </button><button> 🢂 </button></div><div><button> 🢇 </button><button> 🢃 </button><button> 🢆 </button></div></div></body></html>";
+  Serial.begin(9600);
   pinMode(Front_right, OUTPUT);
   pinMode(Front_left, OUTPUT);
   pinMode(Back_left, OUTPUT);
@@ -72,4 +70,8 @@ void setup() {
 }
 
 void loop() {
+	moveMotors("Front");
+  	Serial.print("Front");
+  	moveMotors("Front-left");
+  	Serial.print("Front-left");
 }
